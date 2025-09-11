47debfd0f4c28012e62f458c6a0ee703c4faa3e6
arnicauser01 <arnicauser01@outlook.com>
2025-09-11 12:39:00 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// minimal express app that requires the vulnerable deps so they end up in node_modules
+const express = require('express');
+const _ = require('lodash');
+const serialize = require('serialize-javascript');
+const tar = require('tar');
+const minimist = require('minimist');
+
+const argv = minimist(process.argv.slice(2));
+
+const app = express();
+
+app.get('/', (req, res) => {
+  // use the libraries in trivial ways so npm installs them and scanners detect them
+  const obj = { a: 1, b: 2 };
+  const clone = _.cloneDeep(obj);
+  const s = serialize(clone);
+  res.type('text').send('ok\n' + s);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
